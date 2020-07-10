88311717af834caeecdbbf67259285f28b21f8d3
Johann Schopplich <mail@johannschopplich.com>
2020-07-10 10:34:14 +0200

    rollup.config.js style fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-import bundleSize from 'rollup-plugin-size';
+import bundleSize from "rollup-plugin-size";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import vue from "rollup-plugin-vue";
 import scss from "rollup-plugin-scss";
+import sass from "sass";
 import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 
@@ -24,7 +25,7 @@ export default [
       vue({ css: false }),
       scss({
         output: "index.css",
-        sass: require("sass"),
+        sass,
       }),
       production && babel({ babelHelpers: "bundled" }),
       production && terser(),
